cc7f438feb06469238566840fce1539e950996a5
agreloe <agreloerica@gmail.com>
2023-05-05 19:38:29 -0300

    adjustments :eyes:

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import styles from '@/styles/Footer.module.scss'
 
 export default function Footer() {
   return (
-    <footer className={`${styles.footer} py-2 px-4 flex justify-center min-h-[80px]`}>
-        <ul className='text-center'>
+    <footer className={`py-2 px-4 flex justify-center min-h-[80px]`}>
+        <ul className='text-center sm:text-[12px]'>
             <li>
                 <span>DeRigo Refrigeration Argentina | <a href='mailto: info@derigoargetina.com.ar' target='_blank' rel='noreferrer'>info@derigoargetina.com.ar</a></span>
             </li>
